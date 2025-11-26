1fdc80c501017b2cd94c910c72ab2e9d779a3e80
Adrian Lopez <israelruiz2874@gmail.com>
2025-11-26 12:08:19 -0500

    solución problema 2

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo021.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo021.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
  *
  * @author reroes
  */
-public class Ejemplo02 {
+public class Ejemplo021 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -31,15 +31,13 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) || (si) || (yes) || (y) "
-                    + "si desea salir del ciclo ");
+            System.out.println("Ingrese (s, si, y,yes) si desea salir del ciclo ");
             salida = entrada.nextLine();
 
-            if ( salida.equals("s") || salida.equals("si") || 
-                    salida.equals("yes") || salida.equals("y") ){
-                bandera = false;
+            if (salida.equals("s") || salida.equals("si")
+                    || salida.equals("yes") || salida.equals("yes")) {
+
             }
-            
 
         } while (bandera); // (bandera==true)
 
